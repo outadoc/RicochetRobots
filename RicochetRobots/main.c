7018ca8b3c1bc919aadf014694bfa64a02fd2fc0
outadoc <outadoc@gmail.com>
2013-12-02 15:53:46 +0100

    Commentaires

--- RicochetRobots/main.c
+++ RicochetRobots/main.c
@@ -13,21 +13,34 @@ int main(int argc, const char * argv[]) {
     //initialisation du seed pour la PRNG
     srand((int) time(NULL));
     
+    //initalisation de ncurses
+    //écran
     initscr();
     
+    //init couleurs
     start_color();
+    //utiliser les couleurs par défaut du terminal
     use_default_colors();
     
-    //paires de couleurs
+    //paires de couleurs pour chaque robot
     init_pair(ROBOT_RED, COLOR_WHITE, COLOR_RED);
     init_pair(ROBOT_BLUE, COLOR_WHITE, COLOR_BLUE);
     init_pair(ROBOT_GREEN, COLOR_WHITE, COLOR_GREEN);
     init_pair(ROBOT_GREY, COLOR_BLACK, COLOR_YELLOW);
+    
+    //autres paires de couleurs
     init_pair(10, COLOR_MAGENTA, COLOR_WHITE);
     
+    //saisir de texte bloquante
     timeout(-1);
-    keypad(stdscr, TRUE);
+    //activation des touches fléchées etc.
+    keypad(stdscr, true);
     
     //exécution de la boucle principale
-    return start();
+    start();
+    
+    //fin de la fenêtre
+    endwin();
+    
+    return 0;
 }
