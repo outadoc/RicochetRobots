6aaa0a46ce00d078f3cfb01913252a6ffcee6706
outadoc <outadoc@gmail.com>
2013-12-24 10:01:44 +0100

    Indentation

--- RicochetRobots/struct.h
+++ RicochetRobots/struct.h
@@ -27,7 +27,7 @@ typedef int Direction;  //la direction est un entier
 
 #define BOARD_SIZE              16  //taille d'un plateau de jeu
 #define BUILTIN_BOARDS_COUNT    3   //nombre de grilles prédéfinies
-#define ROBOTS_COUNT       4   //nombre maximal de joueurs
+#define ROBOTS_COUNT            4   //nombre de robots
 #define MAX_USERNAME_SIZE       15  //taille maximale d'un pseudo
 #define MAX_LVL_PATH_SIZE       300 //taille maximale du chemin d'un fichier niveau
 
